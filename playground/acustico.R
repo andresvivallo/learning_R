3a8d9750d8c8d0603d639b3ad54208526632aa41
Pampanito <b.gallardo.sv@gmail.com>
2022-09-07 19:33:52 -0400

    me salio conchetumareeeee

--- playground/acustico.R
+++ playground/acustico.R
@@ -9,15 +9,13 @@ data %>% as_tibble()
 
 #convertir fecha en fecha xd
 data$FECHA <- lubridate::ymd(as.character(data$FECHA))
+data %>% as_tibble()
 
 #calcular maximo
-data %>% as_tibble()
-data %>%
+maximos=data %>%
   group_by(FECHA) %>%
-  summarise(max = max(BI))
-dfrgdfgdfdfgdfg
-dfg
-fdg
-fdg
-dfg
-fd
+  summarise(max = max(BI)) 
+maximos
+
+#graficar
+
