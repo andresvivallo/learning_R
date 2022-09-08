6596a4edb5b3b2ebfdcf6b677d4546add3a210ac
Pampanito <b.gallardo.sv@gmail.com>
2022-09-07 20:20:43 -0400

    tuvieja

--- playground/acustico.R
+++ playground/acustico.R
@@ -11,11 +11,44 @@ data %>% as_tibble()
 data$FECHA <- lubridate::ymd(as.character(data$FECHA))
 data %>% as_tibble()
 
-#calcular maximo
+#calcular maximo diario
 maximos=data %>%
   group_by(FECHA) %>%
   summarise(max = max(BI)) 
 maximos
 
 #graficar
+maximos %>% 
+  ggplot(aes(x=FECHA, y=max)) + geom_bar(stat="identity")
+
+#calcular promedio diario
+media=data %>%
+  group_by(FECHA) %>%
+  summarise(mean = mean(BI)) 
+media
+
+#graficar
+media %>% 
+  ggplot(aes(x=FECHA, y=mean)) + geom_bar(stat="identity")
+
+#calcular maximo diario
+rangohorario=data %>%
+  group_by(FECHA) %>%
+  summarise(max = max(BI)) 
+maximos
+
+#calcular maximo mensual por rango horario
+#1convertir UTC en HORASCL
+dataconvertida <- data %>% 
+  mutate(HORARIO = ifelse(address == '',work_address,address))
+dataconvertida
+
+data$HORARIO <- lubridate::ymd(as.character(data$FECHA))
+
+maximos=data %>%
+  group_by(FECHA) %>%
+  summarise(max = max(BI)) 
+maximos
+
+
 
